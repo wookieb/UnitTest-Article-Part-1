25efb0b0613720b974a897a2bf7ab8eed2263695
Wookieb <wookieb@wp.pl>
2011-06-27 22:07:02 +0200

    usuniecie zbednych plikow

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'autoloader.php';
-set_include_path(get_include_path().PATH_SEPARATOR.'application');
-$reader = new Reader('test.xml');
-print_r($reader->getData());
-print_r($reader->getErrors());
